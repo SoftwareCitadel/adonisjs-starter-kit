6320e7f7f81ec7107c7b2eb36fa35d718d5696d8
Alexis Bouchez <80361585+alexisbouchez@users.noreply.github.com>
2024-08-26 21:59:38 +0200

    Restyle the blog marketing page a lil bit

--- app/marketing/ui/pages/blog.tsx
+++ app/marketing/ui/pages/blog.tsx
@@ -7,7 +7,7 @@ import BlogPost from '#blog/database/models/blog_post'
 export default function Blog({ posts }: { posts: BlogPost[] }) {
   return (
     <MarketingLayout>
-      <div className="min-h-[calc(100vh-86px)] mx-auto max-w-5xl h-full flex flex-col items-center p-6 lg:my-12">
+      <div className="min-h-[calc(100vh-86px)] mx-auto max-w-3xl h-full flex flex-col items-center p-6 lg:my-12">
         <h2 className="font-semibold flex items-center gap-x-2 text-center text-2xl sm:text-3xl">
           <span>Our Latest Articles.</span>
           <IconArticle className="text-zinc-500" />
@@ -16,7 +16,7 @@ export default function Blog({ posts }: { posts: BlogPost[] }) {
           In-depth articles on everything you need to know about
           <br /> web development with AdonisJS.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 border-t py-8 lg:py-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 border-t py-8 lg:py-12 lg:gap-12">
           {posts.map((post) => (
             <BlogPostCard key={post.id} post={post} />
           ))}
